7e971acb8363e84a49823d872ba1b986bd1922bb
Alper Said Soylu <54alpersaid@gmail.com>
2023-04-30 21:18:25 +0300

    Remove cron

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm .env
+touch .env
+
+echo "GITHUB_TOKEN=${GITHUB_TOKEN}" >> .env
+echo "RAINDROP_TOKEN=${RAINDROP_TOKEN}" >> .env
+echo "RAINDROP_COLLECTION_ID=${RAINDROP_COLLECTION_ID}" >> .env
