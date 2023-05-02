96157731432b62ea0328ce3ebb734e6aff4775ac
Alper Said Soylu <54alpersaid@gmail.com>
2023-05-03 00:22:03 +0300

    run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,6 @@ touch .env
 echo "GITHUB_TOKEN=${GITHUB_TOKEN}" >> .env
 echo "RAINDROP_TOKEN=${RAINDROP_TOKEN}" >> .env
 echo "RAINDROP_COLLECTION_ID=${RAINDROP_COLLECTION_ID}" >> .env
+
+npm install
+npm start
